390161e4fa3191463c82331770baa0d6f454b2ac
Tari <tari@Taris-MacBook-Air.local>
2024-10-09 01:30:32 +0900

    swagger 초기 세팅

--- src/main/java/com/dogpalse/demo/auth/controller/UserController.java
+++ src/main/java/com/dogpalse/demo/auth/controller/UserController.java
@@ -5,24 +5,50 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dogpalse.demo.auth.dto.UserDto;
 import com.dogpalse.demo.auth.service.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Slf4j
-@RequestMapping(value = "/api/v1/users")
+@RequestMapping(value = "users")
+@Tag(name = "유저 정보", description = "유저 관련 API")
 @RestController
 public class UserController {
     
     @Autowired
     private UserService userService;
 
-    @GetMapping()
+    @Operation(summary = "유저 목록 조회")
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "",
+            content = {
+                @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE, 
+                    array = @ArraySchema(
+                        schema = @Schema(
+                            implementation = UserDto.class
+                        )
+                    )
+                )
+            }
+        )
+    })
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<UserDto> getMethodName() {
         List<UserDto> userList = userService.selectUserList();
         return userList;
